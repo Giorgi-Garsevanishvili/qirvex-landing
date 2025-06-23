6de31e56e3d31c66249b17ca03946336c69da1bd
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-06-23 13:33:01 +0300

    theme button issue fixed

--- src/components/theme.jsx
+++ src/components/theme.jsx
@@ -1,19 +1,28 @@
 import darkIMG from "../assets/photos/night-mode.png";
 import lightIMG from "../assets/photos/brightness.png";
+import { useState } from "react";
+
 const root = document.getElementById("root");
 
 export const ThemeSwitch = () => {
+  let [dark, setDark] = useState(false);
   const toggle = () => {
     if (root.classList.contains("dark")) {
       root.classList.remove("dark");
+      setDark(false);
     } else {
       root.classList.add("dark");
+      setDark(true);
     }
   };
 
   return (
     <button onClick={() => toggle()} className="theme-btn">
-      <img className="theme-img" src={darkIMG} alt="Dark Theme" />
+      <img
+        className="theme-img"
+        src={dark ? lightIMG : darkIMG}
+        alt="Dark Theme"
+      />
     </button>
   );
 };
